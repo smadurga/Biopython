cef48146bd5c39ddfcdb1a88c640b2dbb375f4b8
S. Madurga <s.madurga@ub.edu>
2017-11-04 21:41:34 +0100

    Update of example4.py

---
+++ example4.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+#
+# Simple program to print ARG residues
+# iteration over residues
+
+from Bio.PDB.PDBParser import PDBParser
+
+
+parser = PDBParser()
+
+st = parser.get_structure('estructura', '1ubq.pdb')
+
+selec=[]
+aa=["ARG"]
+
+for res in st.get_residues():
+    if res.get_resname() in aa:
+        selec.append(res)
+        print(res.get_resname(),res.id)	
+
+print("Coordinates:")
+for res in selec:
+    for atom in res.get_atoms():
+        print(res.get_resname(),res.id,
+        atom.get_name(),atom.get_coord())
