b49def52dbcac41c382145d397ef083a50d7851d
S. Madurga <s.madurga@ub.edu>
2017-11-04 20:19:13 +0100

    Update example2

--- example2.py
+++ example2.py
@@ -6,7 +6,7 @@ from Bio.PDB.NeighborSearch import NeighborSearch
 from Bio.PDB.PDBParser import PDBParser
 
 
-HBLNK=3.5
+HBLNK=3.5  # Define distance for a contact
 
 parser = PDBParser(PERMISSIVE=1)
 
@@ -16,18 +16,17 @@ selecc=[]
 
 for at in st.get_atoms():
     selecc.append(at)
-    print(at)	
-
-print(st)
-print(len(st))
+    print("ATOM:",at)	
 
 #Selecting all atoms.
 nbsearch=NeighborSearch(selecc)
 
 print("NBSEARCH:")
 #Searching for contacts under HBLNK
+ncontact=1
 for at1,at2 in nbsearch.search_all(HBLNK):
-    print("at1:",at1,"at2:",at2)
+    print("Contact: ",ncontact)
     print("at1",at1,at1.get_serial_number(),at1.get_parent().get_resname())
     print("at2",at2,at2.get_serial_number(),at2.get_parent().get_resname())
     print()
+    ncontact+=1
