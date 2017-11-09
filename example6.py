3b35c712180d615aee96f90191e189707b651789
smadurga <33314419+smadurga@users.noreply.github.com>
2017-11-09 14:57:50 +0100

    Example6: ResidueDepth

---
+++ example6.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+#
+# Simple program to print distances between residues
+
+from Bio.PDB.PDBParser import PDBParser
+import Bio.PDB.ResidueDepth as ResidueDepth
+import numpy as np
+
+
+parser = PDBParser()
+
+st = parser.get_structure('estructura', '1ubq.pdb')
+
+print("\nResidue.     (Avg. Depth, CA Depth)")
+resdepth=ResidueDepth(st)
+for resInfo , depth in resdepth:
+    print(resInfo,depth)
+
